5719a0609ba7c6af69a3a273ec609e4abcd1b3c2
James Smith <james@floppy.org.uk>
2012-03-02 17:11:10 +0000

    rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kulaverse::Application.config.secret_token = '5155dec0ce956f72d889bd8db3604947ffd8a2d8e6214ec6f8f3cc9bef24ee9d1e0eec7c7c1a4d0be03176701cd6db9e2366603ee604f924e9e4c1630fef0730'
