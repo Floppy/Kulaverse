26d23a2ade77004a582c321af3604f6dff0cf2e1
James Smith <james@floppy.org.uk>
2012-03-14 20:31:10 +0000

    Fix loading secret from environment

--- config/initializers/_load_twitter_config.rb
+++ config/initializers/_load_twitter_config.rb
@@ -1,6 +1,6 @@
 # Load from env vars (for heroku)
 $twitter_consumer_key = ENV['TWITTER_CONSUMER_KEY']
-$twitter_consumer_secret = ENV['TWITTER_CONSUMER_KEY']
+$twitter_consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
 # If env vars are empty, load from config file
 if $twitter_consumer_key.nil? || $twitter_consumer_secret.nil?
   conf = YAML.load_file("#{Rails.root}/config/twitter.yml")[Rails.env]
