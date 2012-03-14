67bc49c73b3b78eb738829193eeeddb70e593ac3
James Smith <james@floppy.org.uk>
2012-03-14 19:41:00 +0000

    Do not initialise the app when precompiling assets

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,8 @@ module Kulaverse
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
+    config.assets.initialize_on_precompile = false
+
     # Set up generators
     config.generators do |g|
       g.test_framework :rspec
