55e3417d4a0c928a138922cd3f4637b47bbf2b23
James Smith <james@floppy.org.uk>
2012-03-17 15:48:57 +0000

    Don't init app on asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,6 +45,9 @@ Kulaverse::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  # Don't initialise the app when precompiling assets - speeds up heroku deploys
+  config.assets.initialize_on_precompile = false
+
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += ["rails_admin/rails_admin.js", 
                                "rails_admin/rails_admin.css", 
