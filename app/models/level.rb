7392723087c036f4ad16c40f26fb3d34d8e7d6be
James Smith <james@floppy.org.uk>
2013-01-11 23:06:46 +0000

    Load levels from JSON instead of DB
    for now

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,8 +1,26 @@
-class Level < ActiveRecord::Base
-  has_many :scores
+require 'json'
 
-  serialize :blocks
-  serialize :start
-  serialize :entities
-
+class Level
+  
+  attr_accessor :level_num, :world_num, :start, :exit, :keys, :blocks, :entities
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+  
+  def self.where(options)
+    if options[:level_num] && options[:world_num]
+      data = JSON.parse(File.read(File.join(Rails.root, 'app', 'levels', options[:world_num], "#{options[:level_num]}.json")))
+      data[:world_num] = options[:world_num]
+      data[:level_num] = options[:level_num]
+      [Level.new(data)]
+    elsif options[:world_num]
+      
+    else
+      []
+    end
+  end
+  
 end
